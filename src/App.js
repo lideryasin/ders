d5ac941f896395f1ff4774d83d7ca0419a4af01c
lideryasin <32715938+lideryasin@users.noreply.github.com>
2018-07-23 00:05:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,7 @@ class App extends Component {
     super();
 
     firebase.initializeApp({
-      apiKey: "AIzaSyANh1XxGP8J_vM662yHVt35D1m-pKiY6AI",
-      authDomain: "ogrenci-bae5d.firebaseapp.com",
-      databaseURL: "https://ogrenci-bae5d.firebaseio.com",
-      projectId: "ogrenci-bae5d",
-      storageBucket: "ogrenci-bae5d.appspot.com",
-      messagingSenderId: "8287473053"
+
     });
   }
 
